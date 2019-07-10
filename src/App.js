67b213ea9923cb546df8e4115dc7aa19ccef4494
argot <pablotesari@gmail.com>
2019-07-10 20:47:12 -0300

    structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {
+        files: [ "foo0", "foo1" ]
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+            </div>
+        );
+    }
+}
+
+export default App;
